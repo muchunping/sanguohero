f44a8751c08553e19409b43306093eb867c60a7e
mouchunping <mouchunping@chelun.com>
2015-11-06 10:54:45 +0800

    创建角色

---
+++ app/src/main/java/cn/igame/sanguoheros/StartActivity.java
@@ -0,0 +1,98 @@
+package cn.igame.sanguoheros;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.PopupWindow;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
+import cn.igame.sanguoheros.util.Logger;
+import cn.igame.sanguoheros.util.ToastUtil;
+
+public class StartActivity extends AppCompatActivity {
+    private PopupWindow popupWindow;
+
+    private Handler handler = new Handler();
+    private Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+            //创建角色
+            startCreatePlayer();
+        }
+    };
+
+    private int sex = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+
+        handler.postDelayed(runnable, 500);
+    }
+
+    private void startCreatePlayer() {
+        if (popupWindow == null) {
+            popupWindow = new PopupWindow(this);
+            popupWindow.setWidth(ViewGroup.LayoutParams.WRAP_CONTENT);
+            popupWindow.setHeight(ViewGroup.LayoutParams.WRAP_CONTENT);
+            popupWindow.setFocusable(true);
+            popupWindow.setOutsideTouchable(false);
+            popupWindow.setAnimationStyle(R.style.top_to_bottom);
+        }
+
+        //设置角色名和性别
+        @SuppressLint("InflateParams")
+        View view = LayoutInflater.from(this).inflate(R.layout.window_create_player_one, null, false);
+        popupWindow.setContentView(view);
+
+        final EditText editText = (EditText) view.findViewById(R.id.editText);
+        RadioGroup group = (RadioGroup) view.findViewById(R.id.radioGroup);
+        group.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                sex = checkedId == R.id.radioButton1 ? 0 : 1;
+            }
+        });
+        Button button = (Button) view.findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = editText.getText().toString();
+                if(name.isEmpty()) {
+                    ToastUtil.showToast("角色名不能为空");
+                    return;
+                }
+                cratePlayer(name);
+                popupWindow.dismiss();
+            }
+        });
+
+        View parent = findViewById(R.id.contentView);
+        if (parent != null) {
+            popupWindow.showAtLocation(parent, Gravity.CENTER, 0, 0);
+        }
+    }
+
+    private void cratePlayer(String name) {
+        Logger.dL("创建角色{" + name + "," + (sex == 0 ? "男" : "女") + "}");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        handler.removeCallbacks(runnable);
+        if (popupWindow != null && popupWindow.isShowing()) {
+            popupWindow.dismiss();
+        }
+    }
+}
