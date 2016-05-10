c092f7c0bffd01da943cf896591625329300306e
MOUCHUNPING018 <MOUCHUNPING018@pingan.com>
2016-05-10 18:27:07 +0800

    修复战场界面布局错乱的问题

--- app/src/main/java/cn/igame/sanguoheros/view/BattleView.java
+++ app/src/main/java/cn/igame/sanguoheros/view/BattleView.java
@@ -1,14 +1,16 @@
 package cn.igame.sanguoheros.view;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Paint;
-import android.graphics.Rect;
 import android.graphics.RectF;
 import android.util.AttributeSet;
 import android.util.TypedValue;
 import android.view.View;
 
+import cn.igame.sanguoheros.R;
 import cn.igame.sanguoheros.util.Logger;
 
 /**
@@ -16,15 +18,15 @@ import cn.igame.sanguoheros.util.Logger;
  * Created by MOUCHUNPING018 on 2016/2/4.
  */
 public class BattleView extends View {
-    public static final int MAX_NUMBER_BATTLE_ACTOR = 9;
     private static final int COLUMN_NUMBER = 3;
     private static final int ROW_NUMBER = 3;
 
     private Paint mPaint = new Paint();
     private Paint mPaint2 = new Paint();
     private int roundSize;
+    private Bitmap bitmap;
 
-    private RectF[] allActorPosition = new RectF[MAX_NUMBER_BATTLE_ACTOR * 2];
+    private RectF[] allActorPosition = new RectF[COLUMN_NUMBER * ROW_NUMBER * 2];
 
     public BattleView(Context context) {
         super(context);
@@ -44,6 +46,7 @@ public class BattleView extends View {
     private void init() {
         mPaint2.setColor(0xFF00FFCC);
         roundSize = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 60, getResources().getDisplayMetrics());
+        bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.ic_launcher);
     }
 
     @Override
@@ -52,8 +55,8 @@ public class BattleView extends View {
         int validWidth = getMeasuredWidth() - getPaddingLeft() - getPaddingRight();
         int validHeight = getMeasuredHeight() - getPaddingTop() - getPaddingBottom();
         int gapWidth = (validWidth - roundSize * COLUMN_NUMBER) / (COLUMN_NUMBER + 1);
-        int gapHeight = (int) ((validHeight * 0.8 - roundSize * ROW_NUMBER) / (COLUMN_NUMBER + 1));
-        measureActorPosition(getPaddingLeft(), getPaddingTop(), getRight() - getPaddingRight(), getBottom() - getPaddingBottom(), gapWidth, gapHeight);
+        int gapHeight = (int) ((validHeight * 0.4 - roundSize * ROW_NUMBER) / (COLUMN_NUMBER + 1));
+        measureActorPosition(getPaddingLeft(), getPaddingTop(), getPaddingLeft() + validWidth, getPaddingTop() + validHeight, gapWidth, gapHeight);
     }
 
     @Override
@@ -64,35 +67,35 @@ public class BattleView extends View {
 
     private void measureActorPosition(int left, int top, int right, int bottom, int gapWidth, int gapHeight) {
         //measure above
-        int leftAbove = left + gapWidth;
-        int topAbove = top + gapHeight;
         for (int i = 0; i < ROW_NUMBER; i++) {
-            topAbove += i * (roundSize + gapHeight);
+            int topAbove = top + gapHeight + i * (roundSize + gapHeight);
             for (int j = 0; j < COLUMN_NUMBER; j++) {
-                leftAbove += j * (roundSize + gapWidth);
-                allActorPosition[i * COLUMN_NUMBER + j] = new RectF(leftAbove, topAbove, left + roundSize, topAbove + roundSize);
+                int leftAbove = left + gapWidth + j * (roundSize + gapWidth);
+                allActorPosition[i * COLUMN_NUMBER + j] = new RectF(leftAbove, topAbove, leftAbove + roundSize, topAbove + roundSize);
                 Logger.dL(allActorPosition[i * COLUMN_NUMBER + j].toString());
             }
         }
 
-        int rightBelow = right - gapWidth;
-        int bottomBelow = bottom - gapHeight;
         for (int i = ROW_NUMBER - 1; i >= 0; i--) { //2,1,0
-            bottomBelow -= i * (roundSize + gapHeight);
-            for (int j = COLUMN_NUMBER; j >= 0; j--) {
-                rightBelow -= j * (roundSize + gapWidth);
-                allActorPosition[i * COLUMN_NUMBER + j + 9] = new RectF(rightBelow - roundSize, bottomBelow - roundSize, rightBelow, bottomBelow);
-                Logger.dL(allActorPosition[i * COLUMN_NUMBER + j].toShortString());
+            int bottomBelow = bottom - gapHeight - i * (roundSize + gapHeight);
+            for (int j = COLUMN_NUMBER - 1; j >= 0; j--) {
+                int rightBelow = right - gapWidth - j * (roundSize + gapWidth);
+                int index = (2 - i) * COLUMN_NUMBER + 2 - j + COLUMN_NUMBER * ROW_NUMBER;
+                allActorPosition[index] = new RectF(rightBelow - roundSize, bottomBelow - roundSize, rightBelow, bottomBelow);
+                Logger.dL(allActorPosition[index].toShortString());
             }
         }
     }
 
     private void drawAllActorPosition(Canvas canvas) {
-        for (RectF rect : allActorPosition) {
+        for (int i = 0; i < allActorPosition.length; i++) {
+            RectF rect = allActorPosition[i];
             if (rect == null)
                 continue;
-            canvas.drawOval(rect, mPaint);
-            canvas.drawOval(rect, mPaint2);
+            RectF rectCopy = new RectF(rect.left, rect.top + rect.height() * 0.7f, rect.right, rect.bottom);
+            canvas.drawOval(rectCopy, mPaint);
+            canvas.drawBitmap(bitmap, null, rect, mPaint);
+            canvas.drawText(String.valueOf(i), rect.centerX(), rect.centerY(), mPaint);
         }
     }
 }
