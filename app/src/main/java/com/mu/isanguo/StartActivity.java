6c665b0ca5ce7cb8a4f3cdb49678c20aeaae2a13
muchunping-home <muchunping@gmail.com>
2017-07-17 22:25:26 +0800

    update

--- app/src/main/java/com/mu/isanguo/StartActivity.java
+++ app/src/main/java/com/mu/isanguo/StartActivity.java
@@ -1,15 +1,17 @@
 package com.mu.isanguo;
 
+import android.app.Fragment;
 import android.content.SharedPreferences;
-import android.os.Handler;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.mu.isanguo.core.SgWorld;
 import com.mu.isanguo.model.player.PeoplePlayer;
+import com.mu.isanguo.ui.CreatePeopleFragment;
 
 public class StartActivity extends AppCompatActivity {
+    public static final String FRAGMENT_CREATE_PEOPLE = "create people";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,7 +43,13 @@ public class StartActivity extends AppCompatActivity {
     }
 
     private void createPeople() {
-
+        Fragment fragment = getFragmentManager().findFragmentByTag(FRAGMENT_CREATE_PEOPLE);
+        if (fragment == null) {
+            fragment = new CreatePeopleFragment();
+            getFragmentManager().beginTransaction().replace(R.id.root_layout, fragment, FRAGMENT_CREATE_PEOPLE).commit();
+        }else {
+            getFragmentManager().beginTransaction().show(fragment).commit();
+        }
     }
 
     private void enterWorld() {
--- app/src/main/java/com/mu/isanguo/ui/CreatePeopleFragment.java
+++ app/src/main/java/com/mu/isanguo/ui/CreatePeopleFragment.java
@@ -1,10 +1,82 @@
 package com.mu.isanguo.ui;
 
 import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.IdRes;
+import android.support.annotation.Nullable;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+
+import com.mu.isanguo.R;
 
 /**
  * !Created by muchunping on 2017/7/10.
  */
 
 public class CreatePeopleFragment extends Fragment {
+    private TextView nameView;
+    private ImageView zhanshiView, fashiView, daoshiView;
+
+    private int type = 0;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_create_people, container, false);
+        nameView = (TextView) view.findViewById(R.id.edit_name);
+        view.findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name = nameView.getText().toString();
+                Log.d("CreatePeople", name + ":" + type);
+            }
+        });
+        zhanshiView = (ImageView) view.findViewById(R.id.imageView0);
+        fashiView = (ImageView) view.findViewById(R.id.imageView1);
+        daoshiView = (ImageView) view.findViewById(R.id.imageView2);
+        zhanshiView.setOnClickListener(clickListener);
+        fashiView.setOnClickListener(clickListener);
+        daoshiView.setOnClickListener(clickListener);
+        ((RadioGroup) view.findViewById(R.id.radioGroup)).setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, @IdRes int i) {
+                if (i == R.id.radioButton0) {
+                    zhanshiView.setImageResource(R.mipmap.nan_zhan_shi);
+                    fashiView.setImageResource(R.mipmap.nan_fa_shi);
+                    daoshiView.setImageResource(R.mipmap.nan_dao_shi);
+                } else {
+                    zhanshiView.setImageResource(R.mipmap.nv_zhan_shi);
+                    fashiView.setImageResource(R.mipmap.nv_fa_shi);
+                    daoshiView.setImageResource(R.mipmap.nv_dao_shi);
+                }
+            }
+        });
+
+        return view;
+    }
+
+    private View.OnClickListener clickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            if (view == zhanshiView) {
+                type = 0;
+                fashiView.setSelected(false);
+                daoshiView.setSelected(false);
+            } else if (view == fashiView) {
+                type = 1;
+                zhanshiView.setSelected(false);
+                daoshiView.setSelected(false);
+            } else {
+                type = 2;
+                zhanshiView.setSelected(false);
+                fashiView.setSelected(false);
+            }
+            view.setSelected(true);
+        }
+    };
 }
